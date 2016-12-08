05ac21cf720b8608df1762f7fac69ab8a80ab335
wangliang <wangliang@idealsee.cn>
2016-12-08 10:26:52 +0800

    增加节流函数

--- Lazyload.js
+++ Lazyload.js
@@ -8,7 +8,6 @@
 
     function Lazyload(opts) {
         this.distance = 200;
-        this.time = 30;
         this.elements = $("img.lazy,textarea.lazy");
 
         for (var key in opts) {
@@ -21,19 +20,18 @@
     Lazyload.prototype.init = function () {
         this.destoryElementIfInScreen();
 
-        var timer, self = this;
+        var self = this;
 
-        addEventListener("scroll", function () {
-            timer && clearTimeout(timer);
-            timer = setTimeout(function () {
-                self.destoryElementIfInScreen()
-            }, self.time)
-        });
-
-        addEventListener("resize", function () {
-            timer && clearTimeout(timer);
+        function scrollFun() {
             self.destoryElementIfInScreen()
-        })
+        }
+        
+        function resizeFun() {
+            self.destoryElementIfInScreen()
+        }
+        
+        addEventListener("scroll", throttle(scrollFun, 30, 30));
+        addEventListener("resize", throttle(resizeFun, 30, 30))
     };
 
     //is in screen
@@ -51,24 +49,25 @@
             if ((h + this.distance) > rect.top) {
                 this.loadImg(tag);
                 this.elements.splice(i, 1);
-                i--; len--;
+                i--;
+                len--;
             }
         }
     };
 
     // load image or textarea value
     Lazyload.prototype.loadImg = function (ele) {
-        if(ele.nodeName === "IMG"){
+        if (ele.nodeName === "IMG") {
             var src = ele.getAttribute("data-src");
             ele.setAttribute("src", src)
-        } else if(ele.nodeName === "TEXTAREA") {
+        } else if (ele.nodeName === "TEXTAREA") {
             var val = ele.value, parentNode = ele.parentNode;
             parentNode.innerHTML = val;
         }
 
     };
 
-    function addEventListener(evt, fn){
+    function addEventListener(evt, fn) {
         window.addEventListener ? this.addEventListener(evt, fn, false) : (window.attachEvent)
             ? this.attachEvent('on' + evt, fn) : this['on' + evt] = fn;
     }
@@ -76,5 +75,28 @@
     //return dom array
     function $(query) {
         return [].slice.call(document.querySelectorAll(query));
+    }
+
+    // 简单的节流函数
+    //fun 要执行的函数
+    //delay 延迟
+    //time  在time时间内必须执行一次
+    function throttle(fun, delay, time) {
+        var timeout,
+            startTime = new Date();
+        return function () {
+            var context = this,
+                args = arguments,
+                curTime = new Date();
+            clearTimeout(timeout);
+            // 如果达到了规定的触发时间间隔，触发 handler
+            if (curTime - startTime >= time) {
+                fun.apply(context, args);
+                startTime = curTime;
+                // 没达到触发间隔，重新设定定时器
+            } else {
+                timeout = setTimeout(fun, delay);
+            }
+        };
     }
 }(window);
